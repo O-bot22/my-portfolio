ba6fc31542771a35fea08b782733a0d25c899d5e
O-bot22 <owenrmiller@gmail.com>
2026-06-25 07:50:06 -0400

    refactor: collage definition moved to separate file

---
+++ src/components/robotCollageData.js
@@ -0,0 +1,87 @@
+export const robotCollage = [
+  {
+    src: '/collage/Monster Truck Firewatch.jpeg',
+    caption: 'Monster Truck Firewatch on display',
+    alt: 'Monster Truck Firewatch robot',
+  },
+  {
+    src: '/collage/Mudslide in cage.jpeg',
+    caption: 'Mudslide robot inside a test cage',
+    alt: 'Mudslide robot in a cage',
+  },
+  {
+    src: '/collage/NERF Turret.jpeg',
+    caption: 'NERF turret prototype ready for field use',
+    alt: 'NERF turret robot prototype',
+  },
+  {
+    src: '/collage/NHRL Workhorse Bot.jpeg',
+    caption: 'NHRL workhorse bot during testing',
+    alt: 'NHRL workhorse robot',
+  },
+  {
+    src: '/collage/Custom Arduino Boat.jpeg',
+    caption: 'Custom Arduino boat with sensor payload',
+    alt: 'Arduino powered custom boat',
+  },
+  {
+    src: '/collage/Printed Transmission.jpeg',
+    caption: '3D printed transmission component',
+    alt: 'Printed transmission part',
+  },
+  {
+    src: '/collage/Engineering Design Foam Car.jpeg',
+    caption: 'Foam car concept from engineering design',
+    alt: 'Engineering design foam car model',
+  },
+  {
+    src: '/collage/VEX bot.jpeg',
+    caption: 'VEX competition bot build',
+    alt: 'VEX competition robot',
+  },
+  {
+    src: '/collage/VEX Team Poster.jpeg',
+    caption: 'VEX team poster celebrating robotics competition achievements',
+    alt: 'VEX team poster',
+  },
+  {
+    src: '/collage/Open Mudslide.jpeg',
+    caption: 'Open chassis view of the Mudslide robot',
+    alt: 'Open Mudslide robot chassis',
+  },
+  {
+    src: '/collage/FRC Team Award.jpeg',
+    caption: 'Award recognition from the FRC team',
+    alt: 'FRC team award',
+  },
+  {
+    src: '/collage/Foamboard Firewatch.jpeg',
+    caption: 'Firewatch concept on foamboard presentation',
+    alt: 'Foamboard Firewatch design',
+  },
+  {
+    src: '/collage/Deltawing Firewatch.jpeg',
+    caption: 'Deltawing Firewatch aerodynamic prototype',
+    alt: 'Deltawing Firewatch project',
+  },
+  {
+    src: '/collage/Crash Firewatch.jpeg',
+    caption: 'Crash test image from Firewatch development',
+    alt: 'Crash Firewatch test',
+  },
+  {
+    src: '/collage/CFD of turbine.jpeg',
+    caption: 'CFD simulation of a turbine design',
+    alt: 'CFD of turbine',
+  },
+  {
+    src: '/collage/Better Combat Bot.jpeg',
+    caption: 'Improved combat bot prototype in action',
+    alt: 'Better Combat Bot prototype',
+  },
+  {
+    src: '/collage/Arduino Mario Screen.jpeg',
+    caption: 'Arduino-powered Mario display project',
+    alt: 'Arduino Mario screen',
+  },
+];
