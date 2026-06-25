536256f2d490ce427f9117a26ea6233b9b536fe5
O-bot22 <owenrmiller@gmail.com>
2026-06-25 10:37:44 -0400

    feat: Firefly Sentry description

--- src/components/projectDefinitions.js
+++ src/components/projectDefinitions.js
@@ -6,7 +6,7 @@ export const projectDefinitions = [
       startDate: '2024-08',
       endDate: 'Present',
       paragraphs: ['As division lead, I led a team of 40 engineering students to build an autonomous rover conformant to Space Dynamics Labratory (SDL) and NASA CubeSat specifications. The rover and payload lander vehicle flew in the WPI High Power Rocketry Club\'s supersonic 5" rocket up to 19,000 ft at the 2026 ESRA International Rocket Engineering Competition alongside 142 other teams from 13 countries. As a team mentor, I will assist new members and senior executives alike in the development of a rover capable of true 3D mapping and intelligent obstacle avoidance.', 'I oversaw Preliminary and Critical Design Reviews of all hardware and software. I weekly met with the executive board, the club officers, and payload sub-team leads to track progress and deadlines in tehcnical and interpersonal issues.', 'The payload deflated its parachute upon landing, self-righted the rover, released a latch mechanism, and then deployed the rover vehicle. Onboard footage from an OpenMV RT1062 captured the launch and landing sequence. A radio module capable of transmitting live video and receiving override remote control commands over 433MHz was tested up to 1 mile in a city, but not used at the launch. Data was collected on a separate test vehicle from a ToF (Time of Flight) camera, 2D scanning LiDAR, and a Nicla Vision module for analysis and training in the coming year (2026-2027).', 'All hardware was student reasearched and developed, either as 3D prints, CNC\'ed aluminum, or externally machined SLM titanium. Motors designed for use in combat robots (similar to BattleBots) were selected for the drivetrain for their high torque and environmental toughness. For structural components, 3D printed parts used PETG-CF to withstand the 20.8G\'s experienced during launch, and crucially, heat resistance to internal rocket temperatures up to a recorded 157°F'],
-      tech: ['Technology 1', 'Technology 2', 'Technology 3'],
+      tech: [],
       media: [
         {
           src: '/projects/IREC full team.jpg',
@@ -73,7 +73,7 @@ export const projectDefinitions = [
       startDate: '2026-1',
       endDate: 'Present',
       paragraphs: ['Cofounded a direct to consumer robotics startup to develop a vacuum tweezer system for use in electronics assembly. The system is designed to be a low-cost, high-precision alternative to traditional pick-and-place machines, with the goal of making electronics assembly more accessible to hobbyists and small businesses.', 'Led the design and development of the vacuum tweezer system, including the mechanical design, control system, and user manual. Conducted extensive testing and iteration to ensure the system met performance and reliability requirements. Sourced 25 parts across 7 suppliers, including custom machined components, to build the first functional prototype. Implemented safety features to prevent damage to delicate components during operation.', 'Generated a 30 step instruction guide with CAD renderings and photographs to assist users in assembling and operating the vacuum tweezer system. The guide includes detailed instructions for each step, as well as troubleshooting tips and safety precautions.'],
-      tech: ['Technology 1', 'Technology 2', 'Technology 3'],
+      tech: [],
       media: [
         {
           src: '/projects/ToolFull transparent bg.png',
@@ -98,8 +98,8 @@ export const projectDefinitions = [
       category: 'Robotics',
       startDate: '2021-08',
       endDate: '2024-05',
-      paragraphs: ['Overview of your robotics or automation project, emphasizing innovation, technical depth, and real-world applications.'],
-      tech: ['Technology 1', 'Technology 2', 'Technology 3'],
+      paragraphs: ['Long running research project to apply robotics to wildfire detection and supression. Starting as an independent study class in high school, continuing through the 2023 WPI Ignite Innovation pitch challenge where the project was awarded $1,000 in seed funding.', 'The project went through four aircraft prototypes where I learned about CNC machining, 3D printing, and composite layup. The final prototype in 2023 was flight-capable, but determined to be too aerodynamically ineffecient to carry the necessary sensors and fire suppression equipment. Managing a two year project as a high school student with a budget of $400 taught me how to prioritize tasks, keep myself inspired and on track, and break down objectives into minimum possible requirements.','At WPI, my peer, Logan Trexler, and I worked with Curtis Carlson (retired CEO of Stanford Research Institute International), canadian drone company Stinton Aerial Services (regularly works with canadian wildfire management), Doctors James Urban and Albert Simeoni of the WPI Fire Protection Engineering department, and Dr. Carlo Pinciroli of the WPI Robotics Engineering department. Logan and I conducted a literature review of wildfire detection and suppression methods, and developed a new approach to wildland urban interface (WUI) fire detection and suppression using a redundant swarm network of tethered drones and ROVs. Presented on the need, benefit, advantages, and comeptitors to the approach. Within the nine week Ignite Innovation program, we examined a case study of the 2016 Fort McMurray wildfire in Alberta, Canada where 4,000 homes were rendered uninhabitable. We connected with the US wildfire fighting community, in talks with retired head of Cal Fire and local Massachusettss wildfire fighter Monica Nouveau.', 'Although wildfire supression costs totaled $3.5 billion in 2022, the project was put on hold due to its complex logistics and high initial costs. The project is currently in a dormant state, but I am still extremely interested in pursuing it in the future. To me, the project represents applying robotics to one of the most demanding envrionements on earth for the benefit of society.'],
+      tech: [],
       media: [
         {
           src: '/projects/Firefly Sentry System.png',
@@ -110,6 +110,16 @@ export const projectDefinitions = [
           src: '/projects/Firefly Sentry Bot.png',
           type: 'image',
           alt: 'Firefly Sentry project image'
+        },
+        {
+          src: '/projects/Firefly Sentry Stats.png',
+          type: 'image',
+          alt: 'Firefly Sentry project image'
+        },
+        {
+          src: '/projects/fire drone sketch.png',
+          type: 'image',
+          alt: 'Firefly Sentry project concept image'
         },
         {
           src: '/projects/All Foam Firewatch.jpeg',
@@ -125,7 +135,7 @@ export const projectDefinitions = [
       startDate: '202',
       endDate: '202',
       paragraphs: ['Overview of your robotics or automation project, emphasizing innovation, technical depth, and real-world applications.'],
-      tech: ['Technology 1', 'Technology 2', 'Technology 3'],
+      tech: [],
       media: []
     },
     // {
